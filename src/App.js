e7c0df4cfe257a02c80fc06c79d28373ccb4ff41
Chukka Vinay <vinaychukka03@gmail.com>
2025-11-12 12:29:53 +0530

    code changed

--- src/App.js
+++ src/App.js
@@ -4,26 +4,36 @@ import axios from 'axios';
 import UserCard from './UserCard';
 function App() {
   const [data, setData] = useState([]);
-  useEffect(
-    () => {
+  useEffect(() => {
+    const redditURL = "https://www.reddit.com/r/reactjs.json?raw_json=1";
 
-      axios.get("https://www.reddit.com/r/reactjs.json?raw_json=1").then(
-        res => {
-          setData(res.data.data.children);
-        }
-      )
-    }, []
-  )
+    const finalURL = isProduction
+      ? `https://api.allorigins.win/get?url=${encodeURIComponent(redditURL)}`
+      : redditURL;
+
+    axios
+      .get(finalURL)
+      .then((res) => {
+        console.log("Fetched Data:", res.data);
+        // AllOrigins returns data inside 'contents' key
+        const jsonData = isProduction ? JSON.parse(res.data.contents) : res.data;
+        setData(jsonData.data.children);
+      })
+      .catch((err) => {
+        console.error("Error fetching Reddit data:", err);
+      });
+  }, []);
+
   return (
     <>
-      <div style={{backgroundColor:' black'}}>
-        <div style={{display:'flex',justifyContent:'center',alignItems:'center',height:'5rem'}}>
+      <div style={{ backgroundColor: ' black' }}>
+        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '5rem' }}>
           <h2>Contentera Assignment</h2>
         </div>
-        <div style={{display:'flex',flexWrap:'wrap',width:'100%',flexDirection:'column',alignItems:'center'}}>
-        {data.map((card) => (
-          <UserCard key={card.data.title} card={card}/>
-        ))}
+        <div style={{ display: 'flex', flexWrap: 'wrap', width: '100%', flexDirection: 'column', alignItems: 'center' }}>
+          {data.map((card) => (
+            <UserCard key={card.data.title} card={card} />
+          ))}
         </div>
       </div>
     </>
